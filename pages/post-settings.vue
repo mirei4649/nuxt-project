59c8bd77d9bb968b2afa3c8427152eeb44d5dec8
mirei4649 <mirei.maruyama0817@gmail.com>
2025-07-27 16:26:00 +0900

    ？？？

--- pages/post-settings.vue
+++ pages/post-settings.vue
@@ -48,7 +48,6 @@
               <select v-model="form.aiSummary">
                 <option value="on">{{ t('on') }}</option>
                 <option value="off">{{ t('off') }}</option>
-                <h1>aaa</h1>
               </select>
             </div>
             <div class="form-group small">
